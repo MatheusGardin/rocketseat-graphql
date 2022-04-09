22172214b794fa61162039efa96c6cdc9f05390e
MatheusGardin <jackmatheus123@gmail.com>
2022-04-09 15:01:08 -0300

    method to create user;

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,18 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
 import path from 'path';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server';
-import { UserResolver } from "./src/resolvers/UserResolver";
+import { UserResolver } from './src/resolvers/UserResolver';
 
 async function main() {
 	const schema = await buildSchema({
 		resolvers: [
-			UserResolver,
+			UserResolver
 		],
 		emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
 	});
-
 	const server = new ApolloServer({ schema });
-
 	const { url } = await server.listen();
 
 	console.log(`Server in running on ${url}`);
